7dcde7d1673571801f8a2a12e01f339013a6d4cf
Jimmy Lin <b00502013@gmail.com>
2022-04-07 18:01:57 +0800

    MVP

--- local_modules/act/index.js
+++ local_modules/act/index.js
@@ -1,10 +1,52 @@
-const createElement = () => {};
-
-const Fragment = () => {};
-
-const render = (vdomRoot, domRoot) => {
-  console.log(domRoot);
+// VDOM
+const createVdom = (tag, props, ...children) => {
+  console.log(tag, props, children);
 };
 
-export { createElement, Fragment };
+// DOM
+const updateDomStyle = (dom, style) => {
+  for (const [key, value] of Object.entries(style)) {
+    dom.style[key] = value;
+  }
+};
+
+const updateDomAttrs = (dom, attrs) => {
+  for (const [key, value] of Object.entries(attrs)) {
+    if (key === "style") {
+      updateDomStyle(dom, value);
+    }
+  }
+};
+
+const updateDomChildren = (dom, children) => {
+  if (typeof children === "string") {
+    dom.innerText = children;
+  } else {
+    dom.replaceChildren(children);
+  }
+};
+
+const createDom = ({ tagName, attrs, children }) => {
+  const dom = document.createElement(tagName);
+  if (attrs) updateDomAttrs(dom, attrs);
+  if (children) updateDomChildren(dom, children);
+  return dom;
+};
+
+const render = (vdom, dom) => {
+  const h1 = createDom({
+    tagName: "h1",
+    children: "Hello world",
+  });
+  const p = createDom({
+    tagName: "p",
+    children: "This is some style",
+    attrs: {
+      style: { fontSize: "32px", color: "white", backgroundColor: "pink" },
+    },
+  });
+  dom.replaceChildren(h1, p);
+};
+
+export { createVdom };
 export { render };
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/guide/features.html#jsx
 export default defineConfig({
   esbuild: {
-    jsxInject: `import { createElement, Fragment } from "act"`,
-    jsxFactory: "createElement",
-    jsxFragment: "Fragment",
+    jsxInject: `import { createVdom } from "act"`,
+    jsxFactory: "createVdom",
   },
 });
